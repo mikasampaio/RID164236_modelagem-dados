ff44452ef34a52676ff0e170ea04bc79fc1938e2
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2026-02-01 18:12:28 -0300

    feat: Adiciona middlewares de tratamento de erros e validação com Zod

---
+++ src/middlewares/errorHandling.middleware.ts
@@ -0,0 +1,20 @@
+import type { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { ErrorMessage } from "@/errors";
+
+export function errorHandler(
+  error: Error | ErrorMessage,
+  _: Request,
+  res: Response,
+  __: NextFunction,
+) {
+  if (error instanceof ErrorMessage) {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: error.message,
+  });
+}
---
+++ src/middlewares/validator.middleware.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { type ZodRawShape, z } from "zod";
+import { ErrorMessage } from "@/errors";
+
+export enum QueryParams {
+  QUERY = "query",
+  BODY = "body",
+}
+
+type ValidateParams = {
+  schema: ZodRawShape;
+  type: QueryParams;
+};
+
+export function validator(params: ValidateParams) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = z.object(params.schema).safeParse(req[params.type]);
+
+    if (!result.success) {
+      const errorFormatted = result.error.issues.map(
+        (issue) => `${issue.path.join(".")}: ${issue.message}`,
+      );
+
+      throw new ErrorMessage(errorFormatted, StatusCodes.UNPROCESSABLE_ENTITY);
+    }
+
+    req[params.type] = result.data;
+
+    next();
+  };
+}
